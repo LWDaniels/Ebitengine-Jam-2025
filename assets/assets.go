eb7fe521d14ed0f4db848d542a88ece1d8c1f904
LWDaniels <lawrencedanielsjr@gmail.com>
2025-06-15 18:03:38 -0700

    basic asset loading

---
+++ assets/assets.go
@@ -0,0 +1,23 @@
+package assets
+
+import (
+	"bytes"
+	"image"
+	_ "image/jpeg"
+	_ "image/png" // to enable png decoding
+
+	_ "embed"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+var (
+	//go:embed gopher.png
+	gopher_png []byte
+	Gopher     = newTexture(gopher_png)
+)
+
+func newTexture(byteArr []byte) *ebiten.Image {
+	im, _, _ := image.Decode(bytes.NewReader(byteArr))
+	return ebiten.NewImageFromImage(im)
+}
--- src/scenes/gamescene.go
+++ src/scenes/gamescene.go
@@ -1,12 +1,15 @@
 package scenes
 
-import "github.com/hajimehoshi/ebiten/v2"
+import (
+	"github.com/LWDaniels/Ebitengine-Jam-2025/assets"
+	"github.com/hajimehoshi/ebiten/v2"
+)
 
 type GameScene struct {
 }
 
 func (g *GameScene) Draw(screen *ebiten.Image) {
-
+	screen.DrawImage(assets.Gopher, &ebiten.DrawImageOptions{})
 }
 
 func (g *GameScene) Update() error {
