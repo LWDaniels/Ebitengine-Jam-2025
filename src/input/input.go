010f01666a847c0010ae3bc7eeebb75932c95d5c
LWDaniels <lawrencedanielsjr@gmail.com>
2025-06-16 23:31:33 -0700

    WASD movement

--- src/input/input.go
+++ src/input/input.go
@@ -11,18 +11,18 @@ import (
 
 func Movement() image.Point {
 	dir := image.Pt(0, 0)
-	if inpututil.IsKeyJustPressed(ebiten.KeyArrowRight) {
+	if inpututil.IsKeyJustPressed(ebiten.KeyArrowRight) || inpututil.IsKeyJustPressed(ebiten.KeyD) {
 		dir.X += 1
 	}
-	if inpututil.IsKeyJustPressed(ebiten.KeyArrowLeft) {
+	if inpututil.IsKeyJustPressed(ebiten.KeyArrowLeft) || inpututil.IsKeyJustPressed(ebiten.KeyA) {
 		dir.X -= 1
 	}
 	// preventing diagonal movement
 	if dir.X == 0 {
-		if inpututil.IsKeyJustPressed(ebiten.KeyArrowUp) {
+		if inpututil.IsKeyJustPressed(ebiten.KeyArrowUp) || inpututil.IsKeyJustPressed(ebiten.KeyW) {
 			dir.Y -= 1
 		}
-		if inpututil.IsKeyJustPressed(ebiten.KeyArrowDown) {
+		if inpututil.IsKeyJustPressed(ebiten.KeyArrowDown) || inpututil.IsKeyJustPressed(ebiten.KeyS) {
 			dir.Y += 1
 		}
 	}
